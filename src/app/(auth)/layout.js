8edd04d5aea2693adc697e834a086909e5bf38d3
Razor-eng <rajatmaharana64@gmail.com>
2024-10-28 20:01:11 +0530

    final_updates

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -4,7 +4,7 @@ import AuthHeader from "./components/AuthHeader";
 export default function AuthLayout({ children }) {
     return (
         <AuthContextProvider>
-            <div className="flex flex-col h-screen w-screen">
+            <div className="flex flex-col h-screen w-screen overflow-hidden">
                 <AuthHeader />
                 <div className="flex-1 flex items-center justify-center">
                     {children}
